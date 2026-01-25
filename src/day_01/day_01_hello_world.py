49f3a8f5711ee10fb2959c2f26eb21f027611215
Erick kamanda <erickkama4@gmail.com>
2026-01-25 14:51:01 +0300

    Professional project setup

---
+++ src/day_01/day_01_hello_world.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+while True:
+    user_input=input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        print("Exiting the chat. Goodbye!")
+        break
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview", contents=user_input
+    )
+
+    print(response.text)
+
