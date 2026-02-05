f4e942e67f39fb565102696a6afd3d33577a8470
Erick kamanda <erickkama4@gmail.com>
2026-02-05 20:10:02 +0300

    add persona system prompt and stable generation settings

--- src/p1_chatbot/cli.py
+++ src/p1_chatbot/cli.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-from prompts import SYSTEM_PROMPT as system_prompt
-from tokens import MAX_OUTPUTUT_TOKENS, TEMPERATURE
+from .sys_prompt import SYSTEM_PROMPT
+from .tokens import MAX_OUTPUTUT_TOKENS, TEMPERATURE
 
 load_dotenv()
 
@@ -20,7 +20,7 @@ while True:
         print("Exiting the chat. Goodbye!")
         break
     response = client.models.generate_content(
-        model="gemini-3-flash-preview", contents=[system_prompt, user_input],
+        model="gemini-3-flash-preview", contents=[SYSTEM_PROMPT, user_input],
         config=types.GenerateContentConfig(
         temperature=TEMPERATURE,
         max_output_tokens=MAX_OUTPUTUT_TOKENS
