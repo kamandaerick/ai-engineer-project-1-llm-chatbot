660ec2beb9e30a3544a8cb6b5fada4931b2a6d77
Erick kamanda <erickkama4@gmail.com>
2026-02-10 12:49:33 +0300

    add day 05 prompting demos and cot mode

--- src/p1_chatbot/cli.py
+++ src/p1_chatbot/cli.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-from .sys_prompt import SYSTEM_PROMPT
-from .tokens import MAX_OUTPUTUT_TOKENS, TEMPERATURE
+# from src.p1_chatbot.sys_prompt import SYSTEM_PROMPT
+from src.p1_chatbot.tokens import MAX_OUTPUTUT_TOKENS, TEMPERATURE
 
 load_dotenv()
 
@@ -13,14 +13,20 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 
 client = genai.Client(api_key=GEMINI_API_KEY)
+chat = client.chats.create(model="gemini-2.5-flash")
+
 
 while True:
     user_input=input("You: ")
+
     if user_input.lower() in ["exit", "quit"]:
         print("Exiting the chat. Goodbye!")
         break
-    response = client.models.generate_content(
-        model="gemini-3-flash-preview", contents=[SYSTEM_PROMPT, user_input],
+    if "/cot" in user_input.lower():
+        user_input += "Explain your reasoning step-by-step, then give the final answer.\n\n Start the answer with '[mode] CoT enabled for next turn' and end with '[mode] CoT disabled'"
+
+    response = chat.send_message(
+        user_input,
         config=types.GenerateContentConfig(
         temperature=TEMPERATURE,
         max_output_tokens=MAX_OUTPUTUT_TOKENS
